2f42e08c379d9210a179dad4c52ba006ca9ed760
Cristina Salazar <cristinasalazarm1@gmail.com>
2022-03-17 10:56:53 +0100

    connected to external api

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
-
-const express = require("express");
-
+import fetch from 'node-fetch';
+import express from "express";
+import 'dotenv/config'
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+const SECRET_KEY="ce99411970192212f1a4e3ce28339cf2"
+
+const getWeather = async () => {
+  const weather = await fetch(
+  `https://api.openweathermap.org/data/2.5/weather?q=london&appid=${SECRET_KEY}`
+)
+  const data  = await weather.json();
+  const forecast = data.weather[0].description;
+  const temperature = data.main.temp;
+  const name = data.name;
+  // console.log(`Today's forecast for ${name}: ${forecast}`);
+  // console.log(`It's currently ${temperature}°F `);
+  return data
+}
+
+
+app.get("/api", async (req, res) => {
+ const result = await getWeather();
+ console.log(result)
+  return res.send(result);
 });
 
 app.listen(PORT, () => {
